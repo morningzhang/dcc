24bb2326b20daecb1f8fba3c7e7ef06080e54d4d
zhangliming <149151874@qq.com>
2014-10-24 15:30:02 +0800

    参数传入的不对

--- dcc/main.py
+++ dcc/main.py
@@ -2,6 +2,10 @@ import kafka,MySQLdb,logging,sys
 import kafka_comsumer
 import mysql_loader
 
+kafka_group="dcc_python_group"
+if len(sys.argv)>2:
+    kafka_group=sys.argv[1]
+
 #log
 logging.basicConfig(format='%(asctime)s - %(levelname)s  %(filename)s  [%(lineno)d]  %(threadName)s  %(message)s', datefmt='[%Y-%m-%d %H:%M:%S]',
                     filename='dcc.log', level=logging.INFO)
@@ -16,7 +20,7 @@ threads=[]
 
 topics=[("dcc_impression","impression_count"),("dcc_click","click_count")]
 for topic in topics:
-    comsumer=kafka_comsumer.KafkaConsumer(kafka_client,topic[0],sys.argv[1] or "dcc_python_group")
+    comsumer=kafka_comsumer.KafkaConsumer(kafka_client,topic[0],kafka_group)
     threads.append(comsumer.a_comsume())
     loader=mysql_loader.MysqlLoader(mysql_conn,topic[1])
     threads.append(loader.a_load_from_queue(comsumer.get_msg_queue()))
